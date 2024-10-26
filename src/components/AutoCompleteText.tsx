f188793ff3e11e587f09115f8c9a8c617d9b0d03
Taha-Jamal <tjamal618@gmail.com>
2024-10-26 17:28:08 +0500

    completed

--- src/components/AutoCompleteText.tsx
+++ src/components/AutoCompleteText.tsx
@@ -1,13 +1,26 @@
-import { ChangeEvent, KeyboardEvent, useState } from "react";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { ChangeEvent, KeyboardEvent, useEffect, useState } from "react";
 import "./AutoCompleteText.css";
-import { Course, Faculty, Props, State } from "./types";
+import { Faculty, Props, State } from "./types";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../redux/store";
+import { facultyCount } from "../redux/semetser/semesterSlice";
 
 export default function AutoCompleteText(props: Props) {
+
+	const dispatch = useDispatch<AppDispatch>();
+	const semester = useSelector((state: RootState) => state.semester.semester);
 	const [state, setState] = useState<State>({
 		suggesions: [],
 		text: "",
 		SelectedIndex: 0,
 	});
+
+	useEffect(() => {
+		if (!props.editing) {
+			setState({ ...state, text: "", suggesions: [] });
+		}
+	}, [props.editing]);
 
 	const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
 		//console.log(e.key);
@@ -31,20 +44,21 @@ export default function AutoCompleteText(props: Props) {
 	const onTextChanged = (e: ChangeEvent<HTMLInputElement>) => {
 		const { items } = props;
 		const value = e.currentTarget.value;
-        
+
 		let suggesions: Faculty[] = [];
 		if (value.length > 0) {
 			const regex = new RegExp(`${value}`, "i");
-			suggesions = items.filter((i) => regex.test(i.TeacherName));
+			suggesions = items.filter((i) => regex.test(i.TeacherName) && i.counts[semester] < (i.fulltime ? 4 : 2));
 		}
-        
+
 		setState({ ...state, suggesions, text: value, SelectedIndex: 0 });
 	};
 
 	const suggestionSelected = (value: Faculty) => {
 		console.log("Suggestion Selected: ", value);
 		setState({ ...state, text: "", suggesions: [] });
-		//props.setFaculty(value);
+		dispatch(facultyCount(value.fid));
+		props.onChange(value.TeacherName);
 	};
 
 	const renderSuggestion = () => {
@@ -54,8 +68,8 @@ export default function AutoCompleteText(props: Props) {
 
 		return (
 			<ul>
-				{state.suggesions.map((item, i) => (
-					<li key={i} onClick={() => suggestionSelected(item)} className={i === state.SelectedIndex ? "selected" : ""}>
+				{state.suggesions.sort((a, b) => +b.fulltime - +a.fulltime).map((item, i) => (
+					<li key={i} onClick={() => suggestionSelected(item)} className={(i === state.SelectedIndex ? "selected " : "") + (item.fulltime ? 'permanent' : '')} >
 						{item.TeacherName}
 					</li>
 				))}
@@ -65,8 +79,9 @@ export default function AutoCompleteText(props: Props) {
 
 	return (
 		<div className="AutoCompleteText">
-			<input value={state.text} onChange={onTextChanged} onKeyDown={handleKeyDown} type="text" style={{ width: "185px" }} />
+			<input value={state.text} onChange={onTextChanged} onKeyDown={handleKeyDown} type="text" style={{ width: "185px", ...!props.editing && { border: 'none' } }} />
 			{renderSuggestion()}
 		</div>
 	);
 }
+
--- src/components/types.ts
+++ src/components/types.ts
@@ -1,48 +1,54 @@
 export type Course = {
-	courseid: number;
-	code: string;
-	title: string;
-	crhr: number;
-	semester: number;
+  courseid: number;
+  code: string;
+  title: string;
+  crhr: number;
+  semester: number;
 };
 
 export type Faculty = {
-	fid: number;
-	TeacherName: string;
-	fulltime: boolean;
+  areas: Areas[];
+  fid: number;
+  TeacherName: string;
+  fulltime: boolean;
+  counts: { Fall: number; Spring: number };
 };
 
 export type Props = {
-	items: Faculty[];
+  editing: boolean;
+  items: Faculty[];
+  onChange: (value: string) => void;
+  // onSelect: (value: string) => void;
+  // setFaculty: (value: Faculty) => void;
 };
 
 export type State = {
-	suggesions: Faculty[];
-	text: string;
-	SelectedIndex: number;
+  suggesions: Faculty[];
+  text: string;
+  SelectedIndex: number;
 };
 
 export type Curriculum = {
-	curid: number;
-	cid: number;
-	semno: number;
-	title: string;
+  curid: number;
+  cid: number;
+  semno: number;
+  title: string;
 };
 
 export type Offer = {
-	semno: number;
-	cid: number;
-	fid: number;
-	sec: string;
-	semester: string;
+  semno: number;
+  cid: number;
+  fid: number;
+  sec: string;
+  semester: string;
 };
 
 export type Areas = {
-	cid: number;
-	fid: number;
-}
+  cid: number;
+  fid: number;
+};
 
 export type Assign = {
-    fid: number;
-    assign: number;
-}
+  fid: number;
+  assign: number;
+};
